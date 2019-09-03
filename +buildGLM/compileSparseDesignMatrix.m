dd9c67657f582a027ddae46d15ad76634c0209a5
Adrian Bondy <adrian.bondy@gmail.com>
2019-09-03 18:05:09 -0400

    improvements to compilation of sparse design matrix

--- +buildGLM/compileSparseDesignMatrix.m
+++ +buildGLM/compileSparseDesignMatrix.m
@@ -1,44 +1,34 @@
 function dm = compileSparseDesignMatrix(dspec, trialIndices)
-% Compile information from experiment according to given DesignSpec
-
-expt = dspec.expt;
-subIdxs = buildGLM.getGroupIndicesFromDesignSpec(dspec);
-
-totalT = sum(ceil([expt.trial(trialIndices).duration]/expt.binSize));
-
-growingX = sparse([], [], [], 0, dspec.edim, round(totalT * dspec.edim * 0.001)); % preallocate
-
-trialIndices = trialIndices(:)';
-
-for kTrial = trialIndices
-    nT = ceil(expt.trial(kTrial).duration / expt.binSize); % TODO move?
-    
-    miniX = zeros(nT, dspec.edim); % pre-allocate a dense matrix for each trial
-    
-    for kCov = 1:numel(dspec.covar) % for each covariate
-        covar = dspec.covar(kCov);
-        sidx = subIdxs{kCov};
-        
-        if isfield(covar, 'cond') && ~isempty(covar.cond) && ~covar.cond(expt.trial(kTrial))
-            continue;
-        end
-        
-        stim = covar.stim(expt.trial(kTrial), nT); % either dense or sparse
-        
-        if isfield(covar, 'basis') && ~isempty(covar.basis)
-            miniX(:, sidx) = basisFactory.convBasis(stim, covar.basis, covar.offset);
-        else
-            miniX(:, sidx) = stim;
+    % Compile information from experiment according to given DesignSpec
+    expt = dspec.expt;
+    subIdxs = buildGLM.getGroupIndicesFromDesignSpec(dspec);
+    trialT = expt.binfun([expt.trial(trialIndices).duration]);
+    totalT = sum(trialT);
+    dm.X = zeros(totalT, sum([dspec.covar.edim]));
+    trialIndices = trialIndices(:)';
+    for k = trialIndices    
+        ndx = (sum(trialT(1:k))-(trialT(k)-1)):sum(trialT(1:k));    
+        for kCov = 1:numel(dspec.covar) % for each covariate
+            covar = dspec.covar(kCov);
+            sidx = subIdxs{kCov}; 
+            if isfield(covar, 'cond') && ~isempty(covar.cond) && ~covar.cond(expt.trial(k))
+                continue;
+            end       
+            stim = covar.stim(expt.trial(k), trialT); % either dense or sparse
+            stim = full(stim);
+            if isfield(covar, 'basis') && ~isempty(covar.basis)
+                dm.X(ndx, sidx) = basisFactory.convBasis(stim, covar.basis, covar.offset);
+            else
+                dm.X(ndx, sidx) = stim;
+            end
         end
     end
-    growingX = [growingX; sparse(miniX)]; %#ok<AGROW>
-end
 
-dm.X = growingX;
-dm.trialIndices = trialIndices;
-dm.dspec = dspec;
+    dm.trialIndices = trialIndices;
+    dm.dspec = dspec;
 
-%% Check sanity of the design
-if any(~isfinite(dm.X(:)))
-    warning('Design matrix contains NaN or Inf...this is not good!');
+    %% Check sanity of the design
+    if any(~isfinite(dm.X(:)))
+        warning('Design matrix contains NaN or Inf...this is not good!');
+    end
 end
