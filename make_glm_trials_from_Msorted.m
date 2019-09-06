ac40fb8ae86d2d17b7bb8fe12518dffe27791fab
Adrian Bondy <adrian.bondy@gmail.com>
2019-09-06 13:42:23 -0400

    fix minor bug

--- make_glm_trials_from_Msorted.m
+++ make_glm_trials_from_Msorted.m
@@ -23,14 +23,14 @@ function rawData = make_glm_trials_from_Msorted(Msorted,varargin)
         rawData.param.(fields_to_copy{f}) = Msorted.(fields_to_copy{f});
     end
     other_events = {'cpoke_in','cpoke_out','clicks_on','spoke','left_clicks','right_clicks'};
-    duration = round(diff(params.spikeWindowS.(params.ref_event))*params.samplingFreq);
+    duration = round(diff(params.spikeWindowS)*params.samplingFreq);
         
     %% preallocate structure in memory
     rawData.trial = struct();
     rawData.trial(Msorted.nTrials).duration = duration; % preallocate
 
     for t = 1:Msorted.nTrials
-        trial_start_time = Msorted.Trials.stateTimes.(params.ref_event)(t) + params.spikeWindowS.(params.ref_event)(1);
+        trial_start_time = Msorted.Trials.stateTimes.(params.ref_event)(t) + params.spikeWindowS(1);
         rawData.trial(t).duration = duration;
         rawData.trial(t).ishit = Msorted.Trials.is_hit(t);
         for e=1:length(other_events)
@@ -49,7 +49,7 @@ function rawData = make_glm_trials_from_Msorted(Msorted,varargin)
         end
         rawData.trial(t).pokedR = Msorted.Trials.pokedR(t);
         for c = 1:length(Msorted.tt)
-            rawData.trial(t).(['sptrain',num2str(c)]) = round( (Msorted.spike_time_s.(params.ref_event){c}{t} - params.spikeWindowS.(params.ref_event)(1) ) * params.samplingFreq);
+            rawData.trial(t).(['sptrain',num2str(c)]) = round( (Msorted.spike_time_s.(params.ref_event){c}{t} - params.spikeWindowS(1) ) * params.samplingFreq);
         end
     end
     if params.removeViolations
