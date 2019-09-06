7e880e40c338e033d2d79faed9462986a16d330c
Adrian Bondy <adrian.bondy@gmail.com>
2019-09-06 13:27:06 -0400

    remove dependence on PB_set_constants

--- make_glm_trials_from_Msorted.m
+++ make_glm_trials_from_Msorted.m
@@ -1,27 +1,36 @@
 function rawData = make_glm_trials_from_Msorted(Msorted,varargin)
+    % Makes the rawData trial structure used by neuroGLM from an Msorted
+    % file.
+    % The final structure has fields giving the time of various events on
+    % each trial and the spike times for each unit (those fields are called
+    % "sptrain1",'sptrain2", etc
     %units in s
     %% parse and validate inputs
     p=inputParser;
     p.addParameter('ref_event','cpoke_in',@(x)validateattributes(x,{'char'},{'nonempty'}));
     p.addParameter('samplingFreq',1e3,@(x)validateattributes(x,{'numeric'},{'scalar'}));
     p.addParameter('removeViolations',true,@(x)validateattributes(x,{'logical'},{'scalar'}));
-    p.addParameter('removeStimTrials',true,@(x)validateattributes(x,{'logical'},{'scalar'}));    
+    p.addParameter('removeStimTrials',true,@(x)validateattributes(x,{'logical'},{'scalar'})); 
+    
+    % time relative to reference event over which you include spikes (make sure the window 
+    % over which you include spiking data in your input structure is the same or smaller)    
+    p.addParameter('spikeWindowS',[-1.25 3.25],@(x)validateattributes(x,{'numeric'},{'numel',2})); 
+    
     p.parse(varargin{:});
     params = p.Results;
-    PB_set_constants;
     fields_to_copy = {'rat','sess_date','sess_time','mat_file_name','sessid'};
     for f=1:length(fields_to_copy)
         rawData.param.(fields_to_copy{f}) = Msorted.(fields_to_copy{f});
     end
     other_events = {'cpoke_in','cpoke_out','clicks_on','spoke','left_clicks','right_clicks'};
-    duration = round(diff(kSpikeWindowS.(params.ref_event))*params.samplingFreq);
+    duration = round(diff(params.spikeWindowS.(params.ref_event))*params.samplingFreq);
         
     %% preallocate structure in memory
     rawData.trial = struct();
     rawData.trial(Msorted.nTrials).duration = duration; % preallocate
 
     for t = 1:Msorted.nTrials
-        trial_start_time = Msorted.Trials.stateTimes.(params.ref_event)(t) + kSpikeWindowS.(params.ref_event)(1);
+        trial_start_time = Msorted.Trials.stateTimes.(params.ref_event)(t) + params.spikeWindowS.(params.ref_event)(1);
         rawData.trial(t).duration = duration;
         rawData.trial(t).ishit = Msorted.Trials.is_hit(t);
         for e=1:length(other_events)
@@ -40,7 +49,7 @@ function rawData = make_glm_trials_from_Msorted(Msorted,varargin)
         end
         rawData.trial(t).pokedR = Msorted.Trials.pokedR(t);
         for c = 1:length(Msorted.tt)
-            rawData.trial(t).(['sptrain',num2str(c)]) = round( (Msorted.spike_time_s.(params.ref_event){c}{t} - kSpikeWindowS.(params.ref_event)(1) ) * params.samplingFreq);
+            rawData.trial(t).(['sptrain',num2str(c)]) = round( (Msorted.spike_time_s.(params.ref_event){c}{t} - params.spikeWindowS.(params.ref_event)(1) ) * params.samplingFreq);
         end
     end
     if params.removeViolations
